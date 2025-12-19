7255165615d4a532522273231d0b0ea12ab28f85
Prakhar Srivastava <prakhar.official15@gmail.com>
2025-12-19 14:44:25 +0530

    UnaryOperators.cs

--- core-csharp-practice/gcr-codebase/programming-elements/UnaryOperators.cs
+++ core-csharp-practice/gcr-codebase/programming-elements/UnaryOperators.cs
@@ -1,15 +1,15 @@
 using System;
 
 class UnaryOperators {
-static void Main(string[] args) {
-int a = 5;
-
-Console.WriteLine("a: " + a); // 5
-Console.WriteLine("++a: " + ++a); // 6 (pre-increment)
-Console.WriteLine("a++: " + a++); // 6 (post-increment)
-Console.WriteLine("a: " + a); // 7
-Console.WriteLine("--a: " + --a); // 6 (pre-decrement)
-Console.WriteLine("a--: " + a--); // 6 (post-decrement)
-Console.WriteLine("a: " + a); // 5
-}
+  static void Main(string[] args) {
+    int a = 5;
+    
+    Console.WriteLine("a: " + a); // 5
+    Console.WriteLine("++a: " + ++a); // 6 (pre-increment)
+    Console.WriteLine("a++: " + a++); // 6 (post-increment)
+    Console.WriteLine("a: " + a); // 7
+    Console.WriteLine("--a: " + --a); // 6 (pre-decrement)
+    Console.WriteLine("a--: " + a--); // 6 (post-decrement)
+    Console.WriteLine("a: " + a); // 5
+  }
 }

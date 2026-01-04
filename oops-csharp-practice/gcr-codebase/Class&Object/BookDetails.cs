6586bbfc7fe4a82d4a8cdeee8a181b2f20dab677
Prakhar Srivastava <prakharsrivastava.ps09@gmail.com>
2026-01-04 22:16:05 +0530

    [Prakhar Srivastava] practiced class and object creation

---
+++ oops-csharp-practice/gcr-codebase/Class&Object/BookDetails.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment.Class_Object
+{
+    internal class BookDetails
+    {
+        private string title;
+        private string author;
+        private double price;
+
+        // Constructor
+        public BookDetails(string title, string author, double price)
+        {
+            this.title = title;
+            this.author = author;
+            this.price = price;
+        }
+
+        // Method to display book details
+        public void DisplayDetails()
+        {
+            Console.WriteLine("Book Title: " + title);
+            Console.WriteLine("Author: " + author);
+            Console.WriteLine("Price: " + price);
+        }
+    }
+
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            BookDetails book = new BookDetails("Clean Code", "Robert C. Martin", 550);
+            book.DisplayDetails();
+        }
+    }
+}
---
+++ oops-csharp-practice/gcr-codebase/Class&Object/Circle.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment.Class_Object
+{
+    internal class Circle
+    {
+        private double radius;
+
+        // Constructor
+        public Circle(double radius)
+        {
+            this.radius = radius;
+        }
+
+        // Method to calculate area
+        public double CalculateArea()
+        {
+            return Math.PI * radius * radius;
+        }
+
+        // Method to calculate circumference
+        public double CalculateCircumference()
+        {
+            return 2 * Math.PI * radius;
+        }
+
+        // Method to display circle details
+        public void DisplayDetails()
+        {
+            Console.WriteLine("Radius: " + radius);
+            Console.WriteLine("Area: " + CalculateArea());
+            Console.WriteLine("Circumference: " + CalculateCircumference());
+        }
+    }
+
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Circle circle = new Circle(5);
+            circle.DisplayDetails();
+        }
+    }
+}

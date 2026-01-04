961c8623a75e58d973123522d6a2022e10f8c414
Prakhar Srivastava <prakharsrivastava.ps09@gmail.com>
2026-01-04 22:07:58 +0530

    [Prakhar Srivastava] praticed questions for the keywords like 'this', 'sealed', 'readonly' and 'is' operator

---
+++ oops-csharp-practice/gcr-codebase/c#_Keywords/HospitalManagement.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment.c__Keywords
+{
+    internal class Patient
+    {
+        public static string HospitalName = "City Care Hospital";
+        private static int totalPatients = 0;
+
+        public string Name;
+        public int Age;
+        public string Ailment;
+        public readonly string PatientID;
+
+        public Patient(string name, int age, string ailment, string patientId)
+        {
+            this.Name = name;
+            this.Age = age;
+            this.Ailment = ailment;
+            this.PatientID = patientId;
+            totalPatients++;
+        }
+
+        public static void GetTotalPatients()
+        {
+            Console.WriteLine($"Total Patients: {totalPatients}");
+        }
+
+        public static void DisplayPatientDetails(object obj)
+        {
+            if (obj is Patient p)
+            {
+                Console.WriteLine($"{p.Name} - {p.PatientID} - {p.Age} - {p.Ailment}");
+            }
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Patient p1 = new Patient("Ravi", 45, "Fever", "PT001");
+            Patient p2 = new Patient("Anita", 30, "Cold", "PT002");
+
+            Console.WriteLine(Patient.HospitalName);
+            Patient.DisplayPatientDetails(p1);
+            Patient.DisplayPatientDetails(p2);
+            Patient.GetTotalPatients();
+
+            Console.ReadLine();
+        }
+    }
+}
+
---
+++ oops-csharp-practice/gcr-codebase/c#_Keywords/VehicleRegistration.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment.c__Keywords
+{
+    internal class Vehicle
+    {
+        public static double RegistrationFee = 5000;
+
+        public string OwnerName;
+        public string VehicleType;
+        public readonly string RegistrationNumber;
+
+        public Vehicle(string ownerName, string vehicleType, string registrationNumber)
+        {
+            this.OwnerName = ownerName;
+            this.VehicleType = vehicleType;
+            this.RegistrationNumber = registrationNumber;
+        }
+
+        public static void UpdateRegistrationFee(double newFee)
+        {
+            RegistrationFee = newFee;
+        }
+
+        public static void DisplayVehicleDetails(object obj)
+        {
+            if (obj is Vehicle v)
+            {
+                Console.WriteLine($"{v.OwnerName} - {v.VehicleType} - {v.RegistrationNumber} - Fee:{RegistrationFee}");
+            }
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Vehicle v1 = new Vehicle("Rahul", "Car", "REG1001");
+            Vehicle v2 = new Vehicle("Sneha", "Bike", "REG1002");
+
+            Vehicle.DisplayVehicleDetails(v1);
+            Vehicle.DisplayVehicleDetails(v2);
+
+            Vehicle.UpdateRegistrationFee(6000);
+            Console.WriteLine("Registration Fee Updated");
+
+            Vehicle.DisplayVehicleDetails(v1);
+
+            Console.ReadLine();
+        }
+    }
+}
